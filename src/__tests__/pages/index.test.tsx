89566be1eea7ac298b653f2cb1de15d0b6d58a70
laksby <laksby@icloud.com>
2024-03-15 02:40:41 +0300

    Fixed test timer

--- src/__tests__/pages/index.test.tsx
+++ src/__tests__/pages/index.test.tsx
@@ -3,15 +3,18 @@ import renderer from 'react-test-renderer';
 import IndexPage from '../../pages/index';
 
 faker.seed(123);
-jest.useFakeTimers();
 
 describe('Index page tests', () => {
   test('Page rendering', async () => {
+    jest.useFakeTimers();
     const component = renderer.create(<IndexPage />);
     let tree = component.toJSON();
     expect(tree).toMatchSnapshot('Initial');
 
-    await jest.advanceTimersByTimeAsync(1000);
+    await renderer.act(() => {
+      jest.advanceTimersByTimeAsync(1000);
+    });
+
     tree = component.toJSON();
     expect(tree).toMatchSnapshot('Loaded');
   });
